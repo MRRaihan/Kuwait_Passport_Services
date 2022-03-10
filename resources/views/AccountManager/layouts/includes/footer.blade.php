9d178754d61d76625e0aba123f50ce90d7ce262a
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-03-10 10:49:42 +0600

    footer updated

--- resources/views/AccountManager/layouts/includes/footer.blade.php
+++ resources/views/AccountManager/layouts/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Start Footerbar -->
 <footer class="footer">
-    Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear())</script> | <a target="_blank" href="{{ route('forntend.index') }}">{{ env('APP_NAME', 'Kuwait Passport Service') }}</a> | All Rights Reserved.Developed by <a target="_blank" href="https://www.tfpbd.com/">TFP Solution Bangladesh (LTD)</a>
+    Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear())</script> | <a target="_blank" href="{{ route('forntend.index') }}">{{ env('APP_NAME', 'Kuwait Passport Service') }}</a> | All Rights Reserved.Developed by <a target="_blank" href="https://www.tfpbd.com/">TFP Solutions Bangladesh Ltd.</a>
 </footer>
 <!-- End Footerbar -->
--- resources/views/Admin/layouts/includes/footer.blade.php
+++ resources/views/Admin/layouts/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Start Footerbar -->
 <footer class="footer">
     {{-- 2016 © Xadmino. --}}
-    Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear())</script> | <a target="_blank" href="{{ route('forntend.index') }}">{{ env('APP_NAME', 'Kuwait Passport Service') }}</a> | All Rights Reserved.Developed by <a target="_blank" href="https://www.tfpbd.com/">TFP Solution Bangladesh (LTD)</a>
+    Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear())</script> | <a target="_blank" href="{{ route('forntend.index') }}">{{ env('APP_NAME', 'Kuwait Passport Service') }}</a> | All Rights Reserved.Developed by <a target="_blank" href="https://www.tfpbd.com/">TFP Solutions Bangladesh Ltd.</a>
 
 </footer>
 <!-- End Footerbar -->
