446f3b64c6a904fef477b5a13a287c8eb4e76580
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-03-10 14:57:16 +0600

    currency changed

--- resources/views/Admin/manualPassport/printSticker.blade.php
+++ resources/views/Admin/manualPassport/printSticker.blade.php
@@ -12,7 +12,7 @@ $p_type = $passport->ems;
 
 
     <img height="35" width="300" style="margin-top: 5px;" src="data:image/png;base64,{{ base64_encode($generatorPNG->getBarcode($p_type, $generatorPNG::TYPE_CODE_128)) }}">
-    <b class="bar_text"> {{ $p_type }}658 , AED: {{ number_format((float)$passport->passport_type_fees_total, 3, '.', '') }} ,
+    <b class="bar_text"> {{ $p_type }}658 , KWD: {{ number_format((float)$passport->passport_type_fees_total, 3, '.', '') }} ,
         B: {{ $passport->branch ? substr(strtoupper($passport->branch->name),0,3) : ''}} <br>
         Name :  {{ $passport->name }} <br>
         Service Name: Manual Passport , Phone: {{ $passport->kuwait_phone }}<br>
