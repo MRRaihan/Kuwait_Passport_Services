1a1e160b71ee80ecdd077b90d98f2b26b3478ee6
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2022-03-16 14:00:05 +0600

    code optimized for in PassportOptionsController

--- database/migrations/2021_12_05_094438_create_manual_passports_table.php
+++ database/migrations/2021_12_05_094438_create_manual_passports_table.php
@@ -66,6 +66,7 @@ class CreateManualPassportsTable extends Migration
             $table->string('otp_verify_at')->nullable();
             $table->boolean('status')->default(1);
             $table->string('bio_enrollment_id')->nullable();
+            $table->string('new_mrp_passport_no')->nullable();
             $table->string('remarks')->nullable();
             $table->boolean('remarks_by')->nullable();
             $table->string('model_name')->default('Manual Passport');
